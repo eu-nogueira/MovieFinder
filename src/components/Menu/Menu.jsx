5b02f727879d570124bba6cc9c9eb4be115771f2
eu-nogueira <marcosnogueirapro@gmail.com>
2025-10-01 23:59:56 -0300

    implementação do campo categorias no menu

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,12 +1,26 @@
+import { useState } from 'react'
 import './Menu.css'
 
 function Menu({ input }) {
+  const [categorias, setCategorias] = useState(false)
+
+  function handleCategoria() {
+    setCategorias(!categorias)
+  }
   
   return (
     <div>
         <ul className='navbar'>
             <li>Inicio</li>
-            <li>Categorias</li>
+            <li onClick={handleCategoria}>{categorias ? 
+            <ul className="categoria">
+            <li>Ação</li>
+            <li>Aventura</li>
+            <li>Comédia</li>
+            <li>Drama</li>
+            <li>Terror</li>
+            </ul> :
+            <p>Categorias</p>}</li>
             <li>{input}</li>
         </ul>
     </div>
